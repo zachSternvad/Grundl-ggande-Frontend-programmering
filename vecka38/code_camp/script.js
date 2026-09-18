ccd7d3c155daf21c427240d30c7a24761174b753
zachSternvad <zachery.meck@gmail.com>
2026-09-18 14:49:07 +0200

    CodeCamp: Klar med cykel 3 & 4

--- vecka38/code_camp/script.js
+++ vecka38/code_camp/script.js
@@ -1,4 +1,19 @@
-console.log("Nord Glow");
-console.log("Nord Glow");
-console.log("Nord Glow");
-// Byt till "Nord Glow" — tre ställen att glömma, rörde 3 rader
+const eventName = "Aurora part 2 Live";
+let ticketCount = 0;
+const isVip = false;
+
+console.log(eventName);
+console.log(eventName);
+ticketCount = 10;
+console.log(ticketCount);
+console.log(isVip);
+
+//eventName = "Nord Glow";
+
+
+function totalMinutes(hours, minutes) {
+    return hours * 60 + minutes;
+}
+
+const travel = totalMinutes(2, 15);
+console.log(travel); // 135
